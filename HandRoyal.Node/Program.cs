57f3f8c1105974c23abc86dc833736211cbdf94c
Chanhyuck Ko <limeelbee@gmail.com>
2025-01-22 19:00:31 +0900

    Initial commit

---
+++ HandRoyal.Node/Program.cs
@@ -0,0 +1,47 @@
+using HandRoyal.Node;
+using Libplanet.Node.Extensions;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Logging.AddConsole();
+if (builder.Environment.IsDevelopment())
+{
+    builder.WebHost.ConfigureKestrel(options =>
+    {
+        // Setup a HTTP/2 endpoint without TLS.
+        options.ListenLocalhost(5259, o => o.Protocols =
+            HttpProtocols.Http1AndHttp2);
+        options.ListenLocalhost(5260, o => o.Protocols =
+            HttpProtocols.Http2);
+    });
+
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+    builder.Services.AddAuthorization();
+    builder.Services.AddAuthentication("Bearer").AddJwtBearer();
+}
+
+builder.Services.AddGrpc();
+builder.Services.AddGrpcReflection();
+builder.Services.AddLibplanetNode(builder.Configuration);
+builder.Services.AddHostedService<BlockChainRendererTracer>();
+
+var handlerMessage = """
+                     Communication with gRPC endpoints must be made through a gRPC client. To learn how to
+                     create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909
+                     """;
+using var app = builder.Build();
+
+app.MapGet("/", () => handlerMessage);
+if (builder.Environment.IsDevelopment())
+{
+    app.MapGrpcReflectionService().AllowAnonymous();
+
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapSchemaBuilder("/v1/schema");
+app.MapGet("/schema", context => Task.Run(() => context.Response.Redirect("/v1/schema")));
+
+await app.RunAsync();
---
+++ HandRoyal/ActionLoader.cs
@@ -0,0 +1,14 @@
+﻿using Bencodex.Types;
+using HandRoyal.Actions;
+using Libplanet.Action;
+using Libplanet.Action.Loader;
+
+namespace HandRoyal;
+
+public class ActionLoader : IActionLoader
+{
+    private readonly TypedActionLoader _actionLoader =
+        TypedActionLoader.Create(typeof(ActionBase).Assembly, typeof(ActionBase));
+
+    public IAction LoadAction(long index, IValue value) => _actionLoader.LoadAction(index, value);
+}
