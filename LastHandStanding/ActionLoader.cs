bbf5941058c16c109ec03d5352de11f08c286ef8
s2quake <han0210@netsgo.com>
2025-02-07 10:52:27 +0900

    feat: Add block actions

--- LastHandStanding/ActionLoader.cs
+++ LastHandStanding/ActionLoader.cs
@@ -8,7 +8,7 @@ namespace LastHandStanding;
 public class ActionLoader : IActionLoader
 {
     private readonly TypedActionLoader _actionLoader =
-        TypedActionLoader.Create(typeof(ActionBase).Assembly, typeof(ActionBase));
+        TypedActionLoader.Create(typeof(ActionLoader).Assembly, typeof(IAction));
 
     public IAction LoadAction(long index, IValue value) => _actionLoader.LoadAction(index, value);
 }
