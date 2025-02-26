cee720fd29b3c318637f90a119ff0bd2a4d865ef
s2quake <han0210@netsgo.com>
2025-02-26 18:21:54 +0900

    fix: Validate SessionMetadata

--- src/HandRoyal/States/SessionMetadata.cs
+++ src/HandRoyal/States/SessionMetadata.cs
@@ -1,11 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using HandRoyal.DataAnnotations;
 using HandRoyal.Serialization;
 using Libplanet.Crypto;
 
 namespace HandRoyal.States;
 
 [Model(Version = 1)]
-public sealed record class SessionMetadata
+public sealed record class SessionMetadata : IValidatableObject
 {
     public static SessionMetadata Default { get; } = new SessionMetadata
     {
@@ -26,21 +27,43 @@ public sealed record class SessionMetadata
     [Property(2)]
     public required Address Prize { get; init; }
 
+    [Positive]
     [Property(3)]
     public int MaximumUser { get; set; } = 8;
 
+    [Positive]
     [Property(4)]
     public int MinimumUser { get; set; } = 2;
 
+    [Positive]
     [Property(5)]
     public int RemainingUser { get; set; } = 1;
 
+    [Positive]
     [Property(6)]
     public long StartAfter { get; set; } = 10;
 
+    [Positive]
     [Property(7)]
     public long RoundLength { get; set; } = 5;
 
     [Property(8)]
     public long RoundInterval { get; set; } = 5;
+
+    IEnumerable<ValidationResult> IValidatableObject.Validate(ValidationContext validationContext)
+    {
+        if (MinimumUser > MaximumUser)
+        {
+            yield return new ValidationResult(
+                "MinimumUser cannot be greater than MaximumUser",
+                [nameof(MinimumUser), nameof(MaximumUser)]);
+        }
+
+        if (RemainingUser > MaximumUser)
+        {
+            yield return new ValidationResult(
+                "RemainingUser cannot be greater than MaximumUser",
+                [nameof(RemainingUser), nameof(MaximumUser)]);
+        }
+    }
 }
